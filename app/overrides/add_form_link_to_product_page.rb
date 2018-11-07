706cf7ae7d55e77ed824828d0e3801ba541f2d76
andrea longhi <andrea@spaghetticode.it>
2018-11-07 14:39:57 +0100

    Disable `Deface` overrides on frontend
    Frontend overrides from `Deface` are now disabled by default. They're enabled
    only for testing/documentation purposes.

--- app/overrides/add_form_link_to_product_page.rb
+++ app/overrides/add_form_link_to_product_page.rb
@@ -4,5 +4,6 @@ Deface::Override.new(
   virtual_path: "spree/products/show",
   name: "customer_image_form_link",
   insert_after: '[data-hook="product_show"]',
-  partial: 'spree/products/new_product_customer_image_link'
+  partial: 'spree/products/new_product_customer_image_link',
+  disabled: !Rails.env.test?
 )
